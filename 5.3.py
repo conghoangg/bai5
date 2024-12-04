8ffaacc776dee038ccf236043dd9cd903b3dce1c
conghoangg <hanhh030205@gmail.com>
2024-12-04 08:31:25 +0700

    Add files via upload

---
+++ 5.3.py
@@ -0,0 +1,6 @@
+print("NGUYỄN CÔNG HOÀNG")
+print("MSSV:235752021610013")
+
+import numpy as np 
+x = np.arange(12, 38) 
+print(x) 
---
+++ 5.4.py
@@ -0,0 +1,9 @@
+print("NGUYỄN CÔNG HOÀNG")
+print("MSSV:235752021610013")
+
+import numpy as np
+mang = np.arange(12, 39)
+print("Mảng ban đầu:",mang)
+
+mang_dao_nguoc = np.flip(mang)
+print("Mảng đảo ngược:", mang_dao_nguoc)
